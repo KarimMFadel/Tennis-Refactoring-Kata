8231224e61a1d7fff31945ae833abe065d940ac5
Karim Fadel <karimmm.fadel@gmail.com>
2025-12-24 00:48:25 +0200

    SISS-001: Avoid using the player reference in private method

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -20,29 +20,31 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        if (player1.getScore() == player2.getScore()) {
-            return getScoreInEqualizeState();
-        } else if (player1.getScore() >= 4 || player2.getScore() >= 4) {
-            return getScoreInWinStateAndAdvantageState();
+        int m_score1 = player1.getScore();
+        int m_score2 = player2.getScore();
+
+        if (m_score1 == m_score2) {
+            return getScoreInEqualizeState(m_score1);
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getScoreInWinStateAndAdvantageState(m_score1, m_score2);
         }
-        return getScoreByCombineBothPlayerScores();
+        return getScoreByCombineBothPlayerScores(player1.getDisplayScoreName(),
+                player2.getDisplayScoreName());
 
     }
 
-    private String getScoreInEqualizeState() {
-        String score;
-        score = switch (player1.getScore()) {
+    private String getScoreInEqualizeState(int m_score) {
+        return switch (m_score) {
             case 0 -> "Love-All";
             case 1 -> "Fifteen-All";
             case 2 -> "Thirty-All";
             default -> "Deuce";
         };
-        return score;
     }
 
-    private String getScoreInWinStateAndAdvantageState() {
+    private String getScoreInWinStateAndAdvantageState(int m_score1, int m_score2) {
         String score;
-        int minusResult = player1.getScore() - player2.getScore();
+        int minusResult = m_score1 - m_score2;
         if (minusResult == 1) score = "Advantage player1";
         else if (minusResult == -1) score = "Advantage player2";
         else if (minusResult >= 2) score = "Win for player1";
@@ -50,7 +52,7 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
-    private String getScoreByCombineBothPlayerScores() {
-        return player1.getDisplayScoreName() + "-" + player2.getDisplayScoreName();
+    private String getScoreByCombineBothPlayerScores(String player1ScoreName, String player2ScoreName) {
+        return player1ScoreName + "-" + player2ScoreName;
     }
 }
