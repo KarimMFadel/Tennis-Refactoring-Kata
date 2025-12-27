954357aa6183e2b0beb0e7a8361c6ed10898a55f
Karim Fadel <karimmm.fadel@gmail.com>
2025-12-27 18:31:45 +0200

    SISS-001: split the logic to main 3 parts/methods

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -24,6 +24,15 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
+        score = checkTieInMatch(score);
+
+        score = checkMatchInProgress(score);
+
+        score = checkWinnerInMatch(score);
+        return score;
+    }
+
+    private String checkTieInMatch(String score) {
         if (P1point == P2point && P1point < 4)
         {
             if (P1point==0)
@@ -36,7 +45,10 @@ public class TennisGame2 implements TennisGame
         }
         if (P1point==P2point && P1point>=3)
             score = "Deuce";
-        
+        return score;
+    }
+
+    private String checkMatchInProgress(String score) {
         if (P1point > 0 && P2point==0)
         {
             if (P1point==1)
@@ -57,11 +69,11 @@ public class TennisGame2 implements TennisGame
                 P2res = "Thirty";
             if (P2point==3)
                 P2res = "Forty";
-            
+
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
-        
+
         if (P1point>P2point && P1point < 4)
         {
             if (P1point==2)
@@ -86,17 +98,20 @@ public class TennisGame2 implements TennisGame
                 P1res="Thirty";
             score = P1res + "-" + P2res;
         }
-        
+        return score;
+    }
+
+    private String checkWinnerInMatch(String score) {
         if (P1point > P2point && P2point >= 3)
         {
             score = "Advantage player1";
         }
-        
+
         if (P2point > P1point && P1point >= 3)
         {
             score = "Advantage player2";
         }
-        
+
         if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
         {
             score = "Win for player1";
@@ -107,7 +122,7 @@ public class TennisGame2 implements TennisGame
         }
         return score;
     }
-    
+
     private void P1Score(){
         P1point++;
     }
