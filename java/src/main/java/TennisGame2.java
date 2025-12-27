8a1dbac525bb83b8025745f4dbf783a8628950f1
Karim Fadel <karimmm.fadel@gmail.com>
2025-12-27 20:31:49 +0200

    SISS-001: refactor checkMatchInProgress by remove duplicated conditions and using switch

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -46,36 +46,20 @@ public class TennisGame2 implements TennisGame
     }
 
     private void checkMatchInProgress(AtomicReference<String> score) {
-        if (P1point>P2point && P1point < 4)
+        if (P2point < 4 && P1point < 4 && P1point != P2point)
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==0)
-                P2res="Love";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score.set(P1res + "-" + P2res);
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==0)
-                P1res="Love";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
+            P1res = switch (P1point) {
+                case 0 -> "Love";
+                case 1 -> "Fifteen";
+                case 2 -> "Thirty";
+                default -> "Forty";
+            };
+            P2res = switch (P2point) {
+                case 0 -> "Love";
+                case 1 -> "Fifteen";
+                case 2 -> "Thirty";
+                default -> "Forty";
+            };
             score.set(P1res + "-" + P2res);
         }
     }
