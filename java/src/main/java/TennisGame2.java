105276a029e08a78ac7542846c632c75d093e4ad
Karim Fadel <karimmm.fadel@gmail.com>
2025-12-28 13:10:06 +0200

    SISS-001:  make functions more abstract

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -23,29 +23,30 @@ public class TennisGame2 implements TennisGame {
     }
 
     public String getScore() {
-        AtomicReference<String> score = new AtomicReference<>("");
+        String score;
 
-        checkTieInMatch(score);
-        checkMatchInProgress(score);
-        if (!checkWinnerInMatch(score)) {
-            checkAdvantageInMatch(score);
-        }
+        score = checkTieInMatch(P1point, P2point);
+        if(score == null) score = checkMatchInProgress(P1point, P2point);
+        if(score == null) score = checkWinnerInMatch(P1point, P2point);
+        if(score == null) score = checkAdvantageInMatch(P1point, P2point);
 
-        return score.toString();
+
+        return score;
     }
 
-    private void checkTieInMatch(AtomicReference<String> score) {
+    private String checkTieInMatch(int P1point, int P2point) {
         if (P1point == P2point) {
-            switch (P1point) {
-                case 0 -> score.set("Love-All");
-                case 1 -> score.set("Fifteen-All");
-                case 2 -> score.set("Thirty-All");
-                default -> score.set("Deuce");
-            }
+            return switch (P1point) {
+                case 0 -> "Love-All";
+                case 1 -> "Fifteen-All";
+                case 2 -> "Thirty-All";
+                default -> "Deuce";
+            };
         }
+        return null;
     }
 
-    private void checkMatchInProgress(AtomicReference<String> score) {
+    private String checkMatchInProgress(int P1point, int P2point) {
         if (P2point < 4 && P1point < 4 && P1point != P2point) {
             P1res = switch (P1point) {
                 case 0 -> "Love";
@@ -59,27 +60,27 @@ public class TennisGame2 implements TennisGame {
                 case 2 -> "Thirty";
                 default -> "Forty";
             };
-            score.set(P1res + "-" + P2res);
+            return P1res + "-" + P2res;
         }
+        return null;
     }
 
-    private boolean checkWinnerInMatch(AtomicReference<String> score) {
+    private String checkWinnerInMatch(int P1point, int P2point) {
         if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-            score.set("Win for player1");
-            return true;
+            return "Win for player1";
         } else if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-            score.set("Win for player2");
-            return true;
+            return "Win for player2";
         }
-        return false;
+        return null;
     }
 
-    private void checkAdvantageInMatch(AtomicReference<String> score) {
+    private String checkAdvantageInMatch(int P1point, int P2point) {
         if (P1point > P2point && P2point >= 3) {
-            score.set("Advantage player1");
+            return "Advantage player1";
         } else if (P2point > P1point && P1point >= 3) {
-            score.set("Advantage player2");
+            return "Advantage player2";
         }
+        return null;
     }
 
     private void P1Score() {
